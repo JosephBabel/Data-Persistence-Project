ce953d8f0004e0783e914d9e7e435617d4037843
JosephBabel <theodorebabel@gmail.com>
2021-06-21 19:15:13 -0700

    ranamed HighScoreManager to MenuManager and added working start and exit buttons

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-public class HighScoreManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
-    public static HighScoreManager Instance;
+    public static MenuManager Instance;
+
+    public string playerName;
 
     void Awake()
     {
@@ -23,6 +29,20 @@ public class HighScoreManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 
     public void SaveHighScore()
